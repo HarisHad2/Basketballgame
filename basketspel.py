71064bda20233cb72215dc3a8523c13bb7502db9
Haris Hadziabdic <haris.hadziabdic@skola.botkyrka.sem>
2020-03-02 09:33:01 +0100

    Create screen

---
+++ basketspel.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+PI = 3.141592653
+
+ORANGECOURT = (   248,   193,   120)
+
+
+size = (700,500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Basketball Game")
