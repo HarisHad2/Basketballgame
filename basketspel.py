c6bead651b5a24d9df92f5682c6e9ccfbfe8c183
Haris Hadziabdic <haris.hadziabdic@skola.botkyrka.sem>
2020-03-05 11:42:22 +0100

    Baskelplanen 1.0

--- basketspel.py
+++ basketspel.py
@@ -3,9 +3,28 @@ pygame.init()
 
 PI = 3.141592653
 
-ORANGECOURT = (   248,   193,   120)
+WHITE = ( 255, 255, 255)
+ORANGECOURT = ( 248, 193, 120)
+BLACK = ( 0, 0, 0)
 
-
-size = (700,500)
+size = (900,600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Basketball Game")
+
+done = False
+
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(WHITE)
+
+    pygame.draw.rect(screen, ORANGECOURT, [0, 300, 900, 300])
+    pygame.draw.ellipse(screen, BLACK, [250, 320, 1200, 260], 20)
+
+    pygame.display.flip()
+
+    clock.tick(60)
