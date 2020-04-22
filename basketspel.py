1da90905b327ecfb5e03a09524b281d471106663
HarisHad2 <57347089+HarisHad2@users.noreply.github.com>
2020-04-22 21:17:53 +0200

    Update basketspel.py

--- basketspel.py
+++ basketspel.py
@@ -7,18 +7,6 @@ WHITE = ( 255, 255, 255)
 ORANGECOURT = ( 248, 193, 120)
 BLACK = ( 0, 0, 0)
 SHIRTNUMBER = (243, 255, 0)
-<<<<<<< HEAD
-
-# Player position
-x_player = 180
-y_player = 430
-
-x_playerspeed = 0
-y_playerspeed = 0
-
-# Screen
-size = (900,600)
-=======
 RED = (255, 0, 0)
 
 # Player position
@@ -27,15 +15,15 @@ x_player = 150
 y_player = 300
 v = 5 
 m = 1
-b = 5
-mb = 1
+
 
 # Ball
 ballImg = pygame.image.load("basketball2.png")
 x_ball = 255
 y_ball = 275
 ballPosX = 0
-
+b = 5
+mb = 1
 
 
 #background
@@ -54,17 +42,12 @@ isjump = False
 
 # Screen
 size = (900,700)
->>>>>>> f5ead737956c42088394f7f7957e4f9251957d77
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Basketball Game")
 
 done = False
 jumping = False
-<<<<<<< HEAD
-gravity = 1.2
-=======
 
->>>>>>> f5ead737956c42088394f7f7957e4f9251957d77
 clock = pygame.time.Clock()
 
 while not done:
@@ -119,42 +102,8 @@ while not done:
     x_ball += ballPosX
     
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if jumping == False:                   
-                    jumping = True
-                    y_playerspeed = -3
-                    if y_player < 200:
-                        y_playerspeed = 3
-
-                
-
-    # --- Game logic
-
-    # The jump
-    if jumping == True:
-        y_player = y_player + gravity
-    y_player = y_player + y_playerspeed
-
     screen.fill(WHITE)
 
-<<<<<<< HEAD
-    # Pitch
-    pygame.draw.rect(screen, ORANGECOURT, [0, 300, 900, 300])
-    pygame.draw.ellipse(screen, BLACK, [250, 320, 1200, 260], 20)
-    pygame.draw.rect(screen, BLACK, [575, 380, 500, 125])
-    pygame.draw.rect(screen, ORANGECOURT, [590, 395, 400, 95])
-    pygame.draw.arc(screen, BLACK, [455,395,250,100],  PI/2,     PI, 2)
-    pygame.draw.arc(screen, BLACK,  [455,395,250,100],    PI, 3*PI/2, 2)
-    
-    # Player
-    pygame.draw.rect(screen, BLACK, [x_player, y_player, 50, 50])
-    font = pygame.font.SysFont('Calibri', 25, True, False)
-    number = font.render("04", True, SHIRTNUMBER)
-    screen.blit(number, [190, y_player + 10])
-
-    
-=======
     #background image
     screen.blit(background,(18, -140))
 
@@ -175,13 +124,7 @@ while not done:
 
     #korgen
     pygame.draw.rect(screen, RED, [750,140,100,8])
->>>>>>> f5ead737956c42088394f7f7957e4f9251957d77
 
     pygame.display.flip()
 
     clock.tick(60) 
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> f5ead737956c42088394f7f7957e4f9251957d77
